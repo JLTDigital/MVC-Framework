d3aacd80502b360cd06ebd510d14d29b1b081bfc
JLTDigital <mrjltaft@gmail.com>
2022-06-20 23:53:18 +0100

    Update

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/mvc_framework');
+
+// Site Name
+define('SITENAME', 'MVC_Framework');
